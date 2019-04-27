f8e5f0c00674e3b24c78facd09b29932b89f43de
Slvr99 <bsawii@gmail.com>
2019-04-27 19:46:18 -0400

    Add project files.

---
+++ InfectedNoGun/CustomWeapon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using InfinityScript;
+
+namespace CustomWeapon
+{
+    public class CustomWeapon : BaseScript
+    {
+        public CustomWeapon()
+        {
+            PlayerConnected += CustomWeapon_PlayerConnected;
+        }
+
+        void CustomWeapon_PlayerConnected(Entity obj)
+        {
+            obj.SpawnedPlayer += () => OnPlayerSpawned(obj);
+            if (obj.SessionTeam == "axis")
+            {
+                obj.TakeAllWeapons();
+            }
+        }
+        public void OnPlayerSpawned(Entity player)
+        {
+            if (player.SessionTeam == "axis")
+            {
+                player.TakeAllWeapons();
+                player.GiveWeapon("flare_mp");
+                player.SetOffhandPrimaryClass("flash");
+                player.SetOffhandSecondaryClass("flash");
+                player.SetPerk("specialty_lowprofile", true, true);
+                player.SetPerk("specialty_marathon", true, true);
+                player.SetPerk("specialty_finalstand", true, true);
+            }
+        }
+    }
+}
+
